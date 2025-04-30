c6f9b34f59f1f174be0f1dc3952e2c5fedb13479
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:27:14 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
-#include <string>
+#include <iostream> // Lab2E4
+#include <cmath>
+using namespace std;
+int num;
+int main()
+{
+    while(true){
+             cout <<"Enter number to square: ";
+cin >> num; // get input from user
 
-int main(){
-    std::cout << "Sudoku" << std::endl;
+
+ if(num==0){
+continue;
+}else if(num <0){
+break;
+}
+else{
+    cout << sqrt(num)<<"\n";
+}
+    }
+
+
+
+
     return 0;
 }
