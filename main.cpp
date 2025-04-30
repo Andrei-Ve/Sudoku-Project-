a9d7dba67330faec3fe3dc0b38e567e1d37807c5
v3eti <Veeti.Klof@metropolia.fi>
2025-04-30 22:29:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,6 @@
-#include <iostream> // Lab2E4
+#include <iostream> 
 #include <cmath>
 using namespace std;
-int num;
-int main()
-{
-    while(true){
-             cout <<"Enter number to square: ";
-cin >> num; // get input from user
-
-
- if(num==0){
-continue;
-}else if(num <0){
-break;
-}
-else{
-    cout << sqrt(num)<<"\n";
-}
-    }
-
-
-
 
     return 0;
 }
