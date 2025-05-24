4b86c352afe98b490d8bde66e18a01ae1b9bc7c2
Bevin-Ranasinghe <bevin.ranasinghe@gmail.com>
2025-05-24 13:17:36 +0300

    task3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+<<<<<<< HEAD
 #include <iostream> 
 #include <cmath>
 using namespace std;
 
     return 0;
 }
+=======
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <set>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <memory>
+using namespace std;
+
+class GameComponent {
+public:
+    virtual void display() const = 0;
+    virtual bool isValid() const = 0;
+    virtual ~GameComponent() {}
+};
+>>>>>>> d733ce5 (namespaces)
