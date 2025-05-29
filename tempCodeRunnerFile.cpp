6741a02cc58339001f504dd694482e334c0f0bdb
Andrei-Ve <andrei.velikainen@gmail.com>
2025-05-29 20:56:25 +0300

    what is happening

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+                        // for(int y = 0; y < 3; ++y){
+                        //     for (int x = 0; x < 3; ++x){
+                        //         if(getValue(y, x) == value) {
+                        //             noDoubles = false;
+                        //             break;
+                        //         }
+                        //         else if(validNumbers.count(getValue(row, col)) > 0){
+                        //         }
+                        //     }
+
+                        // }
